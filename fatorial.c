008c81244d035c42abea243cebcc1a1be2e5c0e0
Alysson <alysouza@gmail.com>
2023-08-26 15:50:00 -0300

    adicionando codigo fatorial

---
+++ fatorial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int funcao(double fatorial){
+    if(fatorial == 0 || fatorial ==1){
+        return 1;
+        }
+    else{
+        return fatorial * funcao(fatorial - 1);
+    }
+}
+
+int main(void){
+
+    double numero, resultado;
+
+    printf("Digite um número para ser Fatorado: ");
+    scanf("%lf", &numero);
+
+    if (numero < 0){
+        printf("Número Fatorial não pode ser negativo...");
+    }
+    else{
+        resultado = funcao(numero);
+        printf("O fatorial de %.2lf é %.2lf.\n", numero, resultado);
+    }
+    return 0;
+}
